6aae2eca3777fa8fae7d1a5c3df6701b9dc88c9e
Guillaume Savaton <guillaume.savaton@tiliosys.fr>
2023-04-15 17:27:43 +0200

    Simplify UART testbench for TX

--- tests/UART_tb.sv
+++ tests/UART_tb.sv
@@ -18,6 +18,8 @@ module UART_tb;
     localparam TIMEOUT  = BIT_TIME * 10 + 2;
 
     bit clk, reset, uart_rx, uart_tx;
+    bit ref_rx, ref_tx;
+    bit tx_data_error, tx_flag_error, tx_done;
 
     Bus uart_bus (clk, reset);
 
@@ -28,6 +30,55 @@ module UART_tb;
     );
 
     always #1 clk = ~clk;
+
+    // Reference TX frame generator and checker.
+    initial begin
+        byte_t data;
+
+        ref_tx = 1;
+
+        forever begin
+            @(posedge uart.control_reg.tx_enable)
+
+            data = uart.tx_data_reg;
+            tx_data_error = 0;
+            tx_flag_error = 0;
+            tx_done = 0;
+
+            @(posedge clk)
+
+            for (int n = 0; n < 10; n ++) begin
+                case (n)
+                    0       : ref_tx = 0; // Start
+                    9       : ref_tx = 1; // Stop
+                    default : ref_tx = data[n-1];
+                endcase
+                repeat(DIVISION + 1) begin
+                    @(posedge clk)
+                    if (uart_tx != ref_tx) begin
+                        tx_data_error = 1;
+                    end
+                    if (uart.control_reg.tx_event_flag) begin
+                        tx_flag_error = 1;
+                    end
+                end
+            end
+
+            @(posedge clk)
+
+            if (uart_tx != ref_tx) begin
+                tx_data_error = 1;
+            end
+
+            @(posedge clk)
+
+            if (!uart.control_reg.tx_event_flag) begin
+                tx_flag_error = 1;
+            end
+
+            tx_done = 1;
+        end
+    end
 
     task bus_write(word_t address, word_t data, wstrobe_t wstrobe);
         uart_bus.address = address * 4;
@@ -42,132 +93,47 @@ module UART_tb;
     endtask
 
     task test_tx(string label, byte_t data, bit irq_enable);
-        automatic bit frame_detected = 0;
-        automatic bit stop = 0;
-        automatic bit timing_failed = 0;
-        automatic bit data_failed = 0;
-        automatic bit flag_failed = 0;
+        automatic control_reg_t ctrl  = '{
+            tx_irq_enable : irq_enable,
+            tx_enable     : 1,
+            default       : 0
+        };
 
-        fork
-            begin : initiator
-                automatic control_reg_t ctrl  = '{
-                    tx_irq_enable : irq_enable,
-                    tx_enable     : 1,
-                    default       : 0
-                };
+        // Write and check the baud rate configuration register.
+        bus_write(DIVISION_ADDRESS, DIVISION, 4'b1111);
+        if (uart.division_reg != DIVISION) begin
+            $display("[FAIL] %s: division_reg write failed (value %08h, expected %08h)", label, uart.division_reg, DIVISION);
+            return;
+        end
 
-                // Write and check the baud rate configuration register.
-                bus_write(DIVISION_ADDRESS, DIVISION, 4'b1111);
-                if (uart.division_reg != DIVISION) begin
-                    $display("[FAIL] %s: division_reg write failed (value %08h, expected %08h)", label, uart.division_reg, DIVISION);
-                    stop = 1;
-                    disable initiator;
-                end
+        // Write and check the TX data register.
+        bus_write(TX_DATA_ADDRESS, data, 4'b0001);
+        if (uart.tx_data_reg != data) begin
+            $display("[FAIL] %s: tx_data_reg write failed (value %02h, expected %02h)", label, uart.tx_data_reg, data);
+            return;
+        end
 
-                // Write and check the TX data register.
-                bus_write(TX_DATA_ADDRESS, data, 4'b0001);
-                if (uart.tx_data_reg != data) begin
-                    $display("[FAIL] %s: tx_data_reg write failed (value %02h, expected %02h)", label, uart.tx_data_reg, data);
-                    stop = 1;
-                    disable initiator;
-                end
+        // Write and check the control register, start transmitting.
+        bus_write(CONTROL_ADDRESS, ctrl, 4'b0001);
+        ctrl.tx_enable = 0;
+        if (uart.control_reg != ctrl) begin
+            $display("[FAIL] %s: control_reg clear failed (value %02h, expected %02h)", label, uart.control_reg, ctrl);
+            return;
+        end
 
-                // Write and check the control register, start transmitting.
-                bus_write(CONTROL_ADDRESS, ctrl, 4'b0001);
-                ctrl.tx_enable = 0;
-                if (uart.control_reg != ctrl) begin
-                    $display("[FAIL] %s: control_reg clear failed (value %02h, expected %02h)", label, uart.control_reg, ctrl);
-                    stop = 1;
-                    disable initiator;
-                end
+        @(posedge tx_done)
 
-                // Stop the other threads after the timeout has elapsed.
-                #(TIMEOUT);
-                stop = 1;
+        if (tx_data_error) begin
+            $display("[FAIL] %s: data bit errors (check waveforms)", label);
+            return;
+        end
 
-                // Check correct frame termination, flags and IRQ.
-                ctrl.tx_event_flag = 1;
+        if (tx_flag_error) begin
+            $display("[FAIL] %s: event flag error (check waveforms)", label);
+            return;
+        end
 
-                if (!frame_detected) begin
-                    $display("[FAIL] %s: timeout", label);
-                end
-                else if (uart.control_reg != ctrl) begin
-                    $display("[FAIL] %s: control_reg invalid after TX (value %02h, expected %02h)", label, uart.control_reg, ctrl);
-                end
-                else if (irq_enable && !uart_bus.irq) begin
-                    $display("[FAIL] %s: IRQ was not asserted", label);
-                end
-                else if (timing_failed) begin
-                    $display("[FAIL] %s: data timing errors", label);
-                end
-                else if (data_failed) begin
-                    $display("[FAIL] %s: data bit errors", label);
-                end
-                else if (flag_failed) begin
-                    $display("[FAIL] %s: event flag timing error", label);
-                end
-                else begin
-                    $display("[PASS] %s", label);
-                end
-            end
-            begin : timing_checker
-                automatic time t0, t1;
-                // Wait for start bit
-                @(negedge uart_tx, stop);
-                if (!stop) begin
-                    t0 = $time;
-                    // Next transitions should happen after multiples of DIVISION cycles.
-                    forever begin
-                        @(uart_tx, stop);
-                        if (stop) break;
-                        t1 = $time;
-                        if ((t1 - t0) % BIT_TIME != 0) begin
-                            $display("[WARN] %s: wrong time between TX transitions (%0d)", label, (t1 - t0) / 2);
-                            timing_failed = 1;
-                        end
-                        t0 = t1;
-                    end
-                end
-            end
-            begin : data_checker
-                // Wait for start bit
-                @(negedge uart_tx, stop);
-                if (stop) disable data_checker;
-
-                frame_detected = 1;
-                // Wait until half of the first data bit has been output.
-                #(BIT_TIME * 3 / 2);
-                for (int n = 0; n < 9; n ++) begin
-                    automatic bit expected = n < 8 ? data[n] : 1;
-                    if (uart_tx != expected) begin
-                        $display("[WARN] %s: wrong value for bit %0d at time %0d (%b)", label, n, $time, uart_tx);
-                        data_failed = 1;
-                    end
-                    #(BIT_TIME);
-                end
-            end
-            begin : flag_timing_checker
-                automatic time t0, t1;
-                // Wait for start bit
-                @(negedge uart_tx, stop);
-                if (stop) disable flag_timing_checker;
-
-                t0 = $time;
-                if (uart.control_reg.tx_event_flag) begin
-                    t1 = t0;
-                end
-                else begin
-                    @(posedge uart.control_reg.tx_event_flag, stop);
-                    if (stop) disable flag_timing_checker;
-                    t1 = $time;
-                end
-
-                if (t1 - t0 < 10 * BIT_TIME) begin
-                    $display("[WARN] %s: TX event flag asserted too early", label);
-                    flag_failed = 1;
-                end
-            end
-        join
+        $display("[PASS] %s", label);
     endtask
 
     initial begin
