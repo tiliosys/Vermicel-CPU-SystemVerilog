8713e833fb1853d6071797a35570a138d25405b6
Guillaume Savaton <guillaume.savaton@tiliosys.fr>
2023-04-24 11:03:42 +0200

    Added benchmark: Vermicopy

---
+++ benchmarks/Vermicopy.c
@@ -0,0 +1,26 @@
+
+#include <stdint.h>
+#include <stdlib.h>
+
+volatile uint32_t *const out  = (uint32_t*)0x10000000;
+volatile uint32_t *const tick = (uint32_t*)0x20000000;
+
+void copy(uint8_t *dest, const uint8_t *src, size_t len) {
+    while (len > 0) {
+        *dest = *src;
+        src ++;
+        dest ++;
+        len --;
+    }
+}
+
+const uint8_t str[] = "This string needs to be copied to another location";
+uint8_t buffer[sizeof(str)] = {0};
+
+int main(void) {
+    *tick = 1;
+    copy(buffer, str, sizeof(str));
+    *out = sizeof(str);
+    *tick = 0;
+}
+
