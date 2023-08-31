846587d541d663e5c514218d0e21109f64aef801
Guillaume Savaton <guillaume.savaton@tiliosys.fr>
2023-08-31 16:01:09 +0200

    Refactoring

--- core/Vermipipe.sv
+++ core/Vermipipe.sv
@@ -14,23 +14,23 @@ module Vermipipe (
     import Vermicodes_pkg::*;
     import Vermicel_pkg::*;
 
-    bit tick;
+    bit           tick;
+    bit           stall;
 
     bit           ibus_done;
     bit           dbus_done;
 
-    // Fetch stage
+    // Fetch stage.
     word_t        fetch_rdata_reg;
     bit           fetch_pending_reg;
-    bit           fetch_stall;
     word_t        fetch_pc_reg;
-    word_t        fetch_pc_incr;
+    word_t        fetch_pc_next;
 
-    // Fetch -> Decode registers
+    // Fetch -> Decode registers.
     word_t        decode_rdata_reg;
     word_t        decode_pc_reg;
 
-    // Decode stage
+    // Decode stage.
     instruction_t decode_instr;
     word_t        decode_xs1;
     word_t        decode_xs2;
@@ -39,7 +39,7 @@ module Vermipipe (
     word_t        decode_alu_a;
     word_t        decode_alu_b;
 
-    // Decode -> Execute registers
+    // Decode -> Execute registers.
     word_t        execute_pc_incr_reg;
     instruction_t execute_instr_reg;
     word_t        execute_alu_a_reg;
@@ -47,30 +47,30 @@ module Vermipipe (
     word_t        execute_xs1_reg;
     word_t        execute_xs2_reg;
 
-    // Execute stage
+    // Execute stage.
     word_t        execute_alu_r;
     bit           execute_will_jump;
     word_t        execute_pc_next;
     word_t        execute_xd;
 
-    // Execute -> Memory registers
+    // Execute -> Memory registers.
     instruction_t memory_instr_reg;
     word_t        memory_xs2_reg;
     word_t        memory_alu_r_reg;
     word_t        memory_xd_reg;
 
-    // Memory stage
+    // Memory stage.
     word_t        memory_rdata_reg;
     word_t        memory_rdata;
     bit           memory_pending_reg;
     word_t        memory_load_data;
     word_t        memory_xd;
 
-    // Memory -> Write-back registers
+    // Memory -> Write-back registers.
     instruction_t writeback_instr_reg;
     word_t        writeback_xd_reg;
 
-    // Write-back stage
+    // Write-back stage.
     bit           writeback_en;
 
     /* ------------------------------------------------------------------------- *
@@ -81,6 +81,13 @@ module Vermipipe (
     //   * when a new instruction is available, and no data memory access is in progress,
     //   * as soon as a data memory access is complete, if a new instruction is available.
     assign tick = (!ibus.valid || ibus.ready) && (!dbus.valid || dbus.ready);
+
+    // If the instruction in the Decode stage has a data dependency with a load instruction
+    // currently in the Execute stage, we will stall the pipeline for one cycle.
+    assign stall = execute_instr_reg.has_rd
+                && execute_instr_reg.is_load
+                && (execute_instr_reg.rd == decode_instr.rs1
+                 || execute_instr_reg.rd == decode_instr.rs2);
 
     /* ------------------------------------------------------------------------- *
      * Fetch stage.
@@ -99,25 +106,34 @@ module Vermipipe (
         end
     end
 
-    assign fetch_pc_incr  = fetch_pc_reg + 4;
-
-    assign ibus.valid     = !fetch_stall && !fetch_pending_reg;
-    assign ibus.address   = fetch_pc_reg;
-    assign ibus.lookahead = execute_will_jump ? execute_pc_next : fetch_pc_incr;
+    assign fetch_pc_next  = execute_will_jump ? execute_pc_next
+                          : stall             ? fetch_pc_reg
+                          :                     fetch_pc_reg + 4;
 
     always_ff @(posedge ibus.clk) begin
         if (ibus.reset) begin
-            fetch_pc_reg     <= 0;
+            fetch_pc_reg <= 0;
+        end
+        else if (tick) begin
+            fetch_pc_reg <= fetch_pc_next;
+        end
+    end
+
+    assign ibus.valid     = !stall && !fetch_pending_reg;
+    assign ibus.address   = fetch_pc_reg;
+    assign ibus.lookahead = fetch_pc_next;
+
+    // Fetch -> Decode registers.
+    always_ff @(posedge ibus.clk) begin
+        if (ibus.reset) begin
             decode_pc_reg    <= 0;
             decode_rdata_reg <= WORD_NOP;
         end
         else if (tick) begin
             if (execute_will_jump) begin
-                fetch_pc_reg     <= execute_pc_next;
                 decode_rdata_reg <= WORD_NOP;
             end
-            else if (!fetch_stall) begin
-                fetch_pc_reg     <= fetch_pc_incr;
+            else if (!stall) begin
                 decode_pc_reg    <= fetch_pc_reg;
                 decode_rdata_reg <= ibus_done ? ibus.rdata : fetch_rdata_reg;
             end
@@ -149,13 +165,6 @@ module Vermipipe (
     // A data dependency is detected when the destination register of a previous
     // instruction that is still in the pipeline is a source register of the
     // instruction in the Decode stage.
-    // If the instruction in the Execute stage is a memory load, the Fetch stage
-    // is stalled for one cycle.
-
-    assign fetch_stall = execute_instr_reg.has_rd
-                      && execute_instr_reg.is_load
-                      && (execute_instr_reg.rd == decode_instr.rs1
-                       || execute_instr_reg.rd == decode_instr.rs2);
 
     assign decode_xs1_fwd = execute_instr_reg.has_rd   && execute_instr_reg.rd   == decode_instr.rs1 && !execute_instr_reg.is_load ? execute_xd
                           : memory_instr_reg.has_rd    && memory_instr_reg.rd    == decode_instr.rs1                               ? memory_xd
@@ -170,12 +179,13 @@ module Vermipipe (
     assign decode_alu_a = decode_instr.use_pc  ? decode_pc_reg    : decode_xs1_fwd;
     assign decode_alu_b = decode_instr.use_imm ? decode_instr.imm : decode_xs2_fwd;
 
+    // Decode -> Execute registers.
     always_ff @(posedge ibus.clk) begin
         if (ibus.reset) begin
             execute_instr_reg   <= INSTR_NOP;
         end
         else if (tick) begin
-            execute_instr_reg   <= execute_will_jump || fetch_stall ? INSTR_NOP : decode_instr;
+            execute_instr_reg   <= execute_will_jump || stall ? INSTR_NOP : decode_instr;
             execute_pc_incr_reg <= decode_pc_reg + 4;
             execute_alu_a_reg   <= decode_alu_a;
             execute_alu_b_reg   <= decode_alu_b;
@@ -214,6 +224,7 @@ module Vermipipe (
     assign execute_will_jump = execute_pc_next != execute_pc_incr_reg;
     assign execute_xd        = execute_instr_reg.is_jump ? execute_pc_incr_reg : execute_alu_r;
 
+    // Execute -> Memory registers.
     always_ff @(posedge ibus.clk) begin
         if (ibus.reset) begin
             memory_instr_reg <= INSTR_NOP;
@@ -261,6 +272,7 @@ module Vermipipe (
 
     assign memory_xd = memory_instr_reg.is_load ? memory_load_data : memory_xd_reg;
 
+    // Memory -> Write-back registers.
     always_ff @(posedge ibus.clk) begin
         if (ibus.reset) begin
             writeback_instr_reg <= INSTR_NOP;
